3fb59c5b1a300c06f4369cf55758b3b718eb9036
Peter Mezei <mezeipetister@gmail.com>
2020-10-14 11:36:53 +0200

    ..

--- src/app/admin/partial/snack-bar/snack-bar.component.ts
+++ src/app/admin/partial/snack-bar/snack-bar.component.ts
@@ -12,22 +12,22 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-snack-bar',
-  animations: [
-    trigger('animate', [
-      state('out', style({
-        top: '-50px',
-        transform: 'translateY(-100%)',
-      })),
-      state('in', style({
-      })),
-      transition('in => out', [
-        animate('0.2s')
-      ]),
-      transition('out => in', [
-        animate('0.2s')
-      ]),
-    ]),
-  ],
+  // animations: [
+  //   trigger('animate', [
+  //     state('out', style({
+  //       top: '-50px',
+  //       transform: 'translateY(-100%)',
+  //     })),
+  //     state('in', style({
+  //     })),
+  //     transition('in => out', [
+  //       animate('0.2s')
+  //     ]),
+  //     transition('out => in', [
+  //       animate('0.2s')
+  //     ]),
+  //   ]),
+  // ],
   templateUrl: './snack-bar.component.html',
   styleUrls: ['./snack-bar.component.scss']
 })
@@ -35,28 +35,33 @@ export class SnackBarComponent implements OnInit {
 
   constructor(private snackService: SnackBarService) { }
 
-  text: string = 'Sikeresen mentve';
-  state: string = 'out';
-  subscription$: Subscription;
+  text: string;
+  // state: string = 'out';
+  subscription: Subscription;
+  displaySubscription: number;
 
   ngOnInit() {
-    this.subscription$ = this.snackService.snackMessage$.subscribe(msg => this.displayMessage(msg));
-  }
+    this.subscription = this.snackService.snackMessage$.subscribe(msg => this.displayMessage(msg));
 
-  display() {
-    this.state = 'in';
-    setTimeout(() => {
-      this.state = 'out';
-    }, 1500);
   }
 
   displayMessage(msg: string) {
+    if (this.displaySubscription) {
+      clearTimeout(this.displaySubscription);
+    }
     this.text = msg;
-    this.display();
+    this.displaySubscription = setTimeout(() => {
+      this.text = null;
+    }, 400);
   }
 
   ngOnDestroy() {
-    this.subscription$.unsubscribe();
+    if ( this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    if (this.displaySubscription) {
+      clearTimeout(this.displaySubscription);
+    }
   }
 
 }
